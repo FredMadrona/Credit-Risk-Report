b0b64c5757ec40b861c0ef86e0873c1d10723198
FredMadrona <fredriccamasis.madrona@bicol-u.edu.ph>
2025-03-23 17:28:05 +0800

    fix riskReportOverview widget

--- app/Filament/Widgets/RiskReportOverview.php
+++ app/Filament/Widgets/RiskReportOverview.php
@@ -11,9 +11,9 @@ class RiskReportOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total Applied Loan 2023', '₱' . number_format(RiskReport::whereYear('created_at', 2023)->sum('applied_loan'), 2)),
-            Stat::make('Total Applied Loan 2024', '₱' . number_format(RiskReport::whereYear('created_at', 2024)->sum('applied_loan'), 2)),
-            Stat::make('Total Applied Loan 2025', '₱' . number_format(RiskReport::whereYear('created_at', 2025)->sum('applied_loan'), 2)),
+            Stat::make('Total Applied Loan 2023', '₱' . number_format(RiskReport::whereYear('date_rated', 2023)->sum('applied_loan'), 2)),
+            Stat::make('Total Applied Loan 2024', '₱' . number_format(RiskReport::whereYear('date_rated', 2024)->sum('applied_loan'), 2)),
+            Stat::make('Total Applied Loan 2025', '₱' . number_format(RiskReport::whereYear('date_rated', 2025)->sum('applied_loan'), 2)),
         ];
     }
 }
